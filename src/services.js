ae1f46351ffd33f0b1fe2cb5b8014e57a9282c91
Snoweid <44259475+Snoweid@users.noreply.github.com>
2019-03-18 16:28:16 +0100

    Mye greier

--- src/services.js
+++ src/services.js
@@ -28,6 +28,8 @@ class AnsatteService {
     });
   }
 }
+export let ansatteService = new AnsatteService();
+
 class KundeService {
   getKunde(id, success) {
     connection.query('select * from kunder where brukerid=?', [id], (error, results) => {
@@ -37,6 +39,46 @@ class KundeService {
     });
   }
 }
-export let ansatteService = new AnsatteService();
 
 export let kundeService = new KundeService();
+
+
+class BicycleService {
+  getBikes(success) {
+    connection.query('select * from sykler', (error, results) => {
+      if (error) return console.error(error);
+
+      success(results);
+    });
+  }
+
+  getBike(id, success) {
+    connection.query('select * from sykler where id=?', [id], (error, results) => {
+      if (error) return console.error(error);
+
+      success(results[0]);
+    });
+  }
+
+  updateBike(sykler, success) {
+    connection.query(
+      'update sykler set status=? where id=?',
+      [sykler.status, sykler.id],
+      (error, results) => {
+        if (error) return console.error(error);
+
+        success();
+      }
+    );
+  }
+  addBike(merke, modell, type, status) {
+    connection.query(
+      'insert into sykler set merke=?, modell=?, type=?, status=?',
+      [merke, modell, type, status],
+      (error, results) => {
+        if (error) return console.error(error);
+      }
+    );
+  }
+}
+export let bikeService = new BicycleService();

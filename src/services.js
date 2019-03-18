5ef690f9dbe54e17e54ecb27ecfd60ad3796949b
Tharmika <Tharmika_99@hotmail.com>
2019-03-18 16:07:15 +0100

    hei

--- src/services.js
+++ src/services.js
@@ -17,4 +17,26 @@ class AnsatteService {
     });
   }
 }
+
+class BestillingService{
+  addKunder(fornavn, etternavn, epost, addresse, postnr, poststed, telefon, success) {
+    connection.query("INSERT INTO kunder (fornavn, etternavn, epost, addresse, postnr, poststed, telefon) VALUES (?,?,?,?,?,?,?)", [fornavn, etternavn, epost, addresse, postnr, poststed, telefon], (error, results) => {
+      if(error) return console.error(error);
+    });
+  }
+
+  getKunde(success) {
+    connection.query("select @@IDENTITY as 'IDENTITY'", (error, results) => {
+      if(error) return console.error(error);
+      success(results[0]);
+    });
+  }
+
+  addLeietaker(start, slutt, current, hentested, leveringssted) {
+    connection.query("INSERT INTO leietaker (start, slutt, kunder_brukerid, ansatte_ansattid, hentested, leveringssted ) VALUES (?,?,?,1,?,?)", [start, slutt, current, hentested, leveringssted], (error, results) => {
+      if(error) return console.error(error);
+    });
+  }
+}
 export let ansatteService = new AnsatteService();
+export let bestillingService = new BestillingService();

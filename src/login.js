aec60f986215c3eb0af562dfe4a4e8c504c6a791
WilliHLe <44968694+WilliHLe@users.noreply.github.com>
2019-03-21 12:47:45 +0100

    Update login.js

--- src/login.js
+++ src/login.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { Component } from 'react-simplified';
 import ReactDOM from 'react-dom';
 import { NavLink, HashRouter, Route } from 'react-router-dom';
-import { connection } from './mysql_connection';
+import { connection } from "./mysql_connection"
 import { Card, List, Row, Column, NavBar, Button, Form } from './widgets';
 import { ansatteService } from './services';
 
@@ -16,42 +16,13 @@ const history = createHashHistory(); // Use history.push(...) to programmaticall
 
 class LogIn extends Component{
   ansatte = [];
-  epost = '';
-  passord = '';
+  epost = "";
+  passord = "";
   form = null;
+
 
   render() {
     return (
-<<<<<<< HEAD
-      <div>
-        <div />
-        <form ref={element => (this.form = element)} className="login-form" onSubmit={this.handleSubmit}>
-          <div className="flex-row">
-            <Card>
-              <Form.Label>Epost: </Form.Label>
-              <Form.Input
-                id="username"
-                className="lf--input"
-                placeholder="Epost"
-                type="text"
-                onChange={event => (this.epost = event.target.value)}
-              />
-              <Form.Label>Passord: </Form.Label>
-              <Form.Input
-                id="password"
-                className="lf--input"
-                placeholder="Passord"
-                type="password"
-                onChange={event => (this.passord = event.target.value)}
-              />
-              <Button.Success onClick={this.login}>Logg inn</Button.Success>
-              <a href="aktivbestilling.html">link til aktivbestilling</a>
-            </Card>
-          </div>
-          <div className="flex-row" />
-        </form>
-      </div>
-=======
       <div className="container h-100">
         <div className="d-flex justify-content-center">
 
@@ -87,39 +58,26 @@ class LogIn extends Component{
         </div>
       </div>
 
->>>>>>> 2486480d9188bde97e88e65403c1001a01f0fe2c
     );
+
   }
-  login() {
+  login(){
     if (!this.form.checkValidity()) return;
 
-    ansatteService.getAnsatt(this.epost, this.passord, ansatte => {
-      console.log(ansatte);
-      if (ansatte.length > 0) {
-        this.ansatte = ansatte;
-        //this.ree();
-        alert(
-          'Velkommen ' + this.ansatte[0].fornavn + ' ' + this.ansatte[0].etternavn + ', Epost: ' + this.ansatte[0].epost
-        );
-        window.location.href = './index.html';
-      } else {
-        alert('Skriv inn riktig epost og passord.');
-      }
+    ansatteService.getAnsatt(this.epost,this.passord,ansatte => {
+        console.log(ansatte);
+        if(ansatte.length > 0){
+          this.ansatte = ansatte;
+          //this.ree();
+          alert("Velkommen " + this.ansatte[0].fornavn + " " + this.ansatte[0].etternavn + ", Epost: " + this.ansatte[0].epost);
+          window.location.href = "./index.html";
+
+        } else {
+          alert("Skriv inn riktig epost og passord.")
+        }
     });
   }
+
 }
 
-<<<<<<< HEAD
-ReactDOM.render(
-  <HashRouter>
-    <div>
-      <Route exact path="/" component={Home} />
-
-      <LogIn />
-    </div>
-  </HashRouter>,
-  document.getElementById('root')
-);
-=======
 export default LogIn;
->>>>>>> 2486480d9188bde97e88e65403c1001a01f0fe2c
